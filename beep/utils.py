8e9aefe0c532cb7c40f01dbc7bec587d56a9d661
Ashot Matevosyan <ashmat98@gmail.com>
2022-07-06 15:41:04 +0400

    add sound files

--- beep/utils.py
+++ beep/utils.py
@@ -1,9 +1,14 @@
 from IPython.display import Audio
 
+sounds = {
+    7: 'https://github.com/ashmat98/task-finish-tools/raw/main/beep/sounds/beep-07a.wav',
+    6: 'https://github.com/ashmat98/task-finish-tools/raw/main/beep/sounds/beep-06.wav',
+    9: 'https://github.com/ashmat98/task-finish-tools/raw/main/beep/sounds/beep-09.wav'
+}
 
-def beep():
+def beep(sound=7):
     """
     beeps at run. Useful tool in jupyter notebook.
     """
-    sound_file = ''
+    sound_file = sounds[sound]
     return Audio(sound_file, autoplay=True) 
